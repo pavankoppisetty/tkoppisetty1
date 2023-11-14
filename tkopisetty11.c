8aed39d3826dcf83e5fbc1df0fa2458c5388c37d
pavankoppisetty <tkoppisetty1@toromail.csudh.edu>
2023-11-14 18:54:02 +0000

    Adding for Lab 11

---
+++ tkopisetty11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+	float x,y;
+	printf("Enter a number: ");
+	scanf("%f",&x);
+
+	y = x*x;
+
+	printf("The Squared number is : %f",y);
+	printf("An Apple a day keeps the doctor away");
+}
